f70bbe0af7ef99a217acd2da851ad9fb462d7268
a.yushko <andrjj.jushko@gmail.com>
2023-06-29 19:52:24 +0300

    Add Element plus library and other

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <script>
+import NavigationBar from './components/NavigationBar.vue'
+
+export default {
+  components: { NavigationBar },
+}
 </script>
 
 <template>
-  Hello world
+  <navigation-bar></navigation-bar>
 </template>
 <style scoped>
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import './style.css'
 import App from './App.vue'
+import 'element-plus/dist/index.css'
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
+app.mount("#app");
